bebfa93d2a0fb652bf85a1470daa16d19d16e2bb
irenaProj <irena.shemesh@gmail.com>
2024-10-06 00:11:53 +0000

    update

--- src/view/suggestedItemsHistoryPlot.js
+++ src/view/suggestedItemsHistoryPlot.js
@@ -7,7 +7,6 @@ import { getItemsInEntries } from '../utils/getItemsInEntries';
 const LINE_COLORS = [
     "#44FFD2",
     "#87F6FF",
-    "#DAF5FF",
     "#FFBFA0",
     "#99C2A2",
     "#aa2993",
@@ -19,7 +18,6 @@ const LINE_COLORS = [
     "#8d2b7d",
     "#239e04",
     "#4a7452",
-    "#deb0f5",
     "#2c4b10",
     "#74ee46",
     "#661f08",
@@ -32,6 +30,8 @@ const LINE_COLORS = [
     "#f5b7b1",
     "#a9cce3",
     "#616163",
+    "#DAF5FF",
+    "#deb0f5",
 ]
 
 
@@ -56,31 +56,50 @@ export const suggestedItemsHistoryPlot = ({ suggestedItems, dataGroup, useSupple
 
     const renderLines = () => suggestedItems.map((si, index) => (<Line type="monotone" key={si.number} dataKey={si.number} stroke={LINE_COLORS[index]} />));
 
+    const CustomTooltip = ({ active, payload, label }) => {
+        if (active && payload && payload.length) {
+            const items = payload.sort((p1, p2) => p2.dataKey - p1.dataKey)
+            // `'${item.color}'`
+            const content = items.map(item => (<span className='custom-tooltip-span' key={item.dataKey} style={{ color: `${item.color}` }}>{item.dataKey}</span>))
+
+            return (
+                <div className="custom-tooltip">
+                    <p className="label">
+                        <span className='custom-tooltip-span'>Date: {label}</span>
+                        {content}
+                    </p>
+                </div>
+            );
+        }
+
+        return null;
+    };
+
 
     return (
         <div className="plot-container overflow-x-scroll ">
             <div className="plot">
-            <ResponsiveContainer width="100%" height="100%" >
-                <LineChart
-                    width={500}
-                    height={300}
-                    data={plotData}
-                    margin={{
-                        top: 5,
-                        right: 30,
-                        left: 20,
-                        bottom: 5,
-                    }}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="Date" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
+                <ResponsiveContainer width="100%" height="100%" >
+                    <LineChart
+                        width={500}
+                        height={300}
+                        data={plotData}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="Date" />
+                        <YAxis />
+                        <Tooltip content={<CustomTooltip />} />
+                        <Legend />
 
-                    {renderLines()}
-                </LineChart>
-            </ResponsiveContainer>
+                        {renderLines()}
+                    </LineChart>
+                </ResponsiveContainer>
             </div>
         </div>
     );
